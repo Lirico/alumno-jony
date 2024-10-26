13774cbc22c7f3acb18b5b9fdae28aca416d186a
lirico <dominguezmatiasadrian@gmail.com>
2024-10-26 15:11:41 -0300

    parcialito base

---
+++ prog01Parcial2024S1_Base/js/javascript.js
@@ -0,0 +1,67 @@
+console.log("javascript.js", "👇 Aquí verás el resultado de tu código 👇")
+
+
+let peliculasMasVistas = [
+  {
+    title: "Avatar: The Way of Water",
+    length: 192,
+    actors: ["Sam Worthington", "Zoe Saldana", "Sigourney Weaver", "Kate Winslet"],
+    awards: 2 // 1 Oscar, 1 Globo de Oro
+  },
+  {
+    title: "Top Gun: Maverick",
+    length: 131,
+    actors: ["Tom Cruise", "Miles Teller", "Jennifer Connelly", "Jon Hamm"],
+    awards: 4 // 1 Oscar, 3 Globos de Oro
+  },
+  {
+    title: "Spider-Man: No Way Home",
+    length: 148,
+    actors: ["Tom Holland", "Zendaya", "Benedict Cumberbatch", "Jacob Batalon"],
+    awards: 0 // No recibió premios Oscar ni Globos de Oro
+  },
+  {
+    title: "Black Panther: Wakanda Forever",
+    length: 161,
+    actors: ["Letitia Wright", "Lupita Nyong'o", "Danai Gurira", "Angela Bassett"],
+    awards: 3 // 1 Oscar, 2 Globos de Oro
+  },
+  {
+    title: "The Batman",
+    length: 176,
+    actors: ["Robert Pattinson", "Zoë Kravitz", "Paul Dano", "Colin Farrell"],
+    awards: 0 // No recibió premios Oscar ni Globos de Oro
+  }
+];
+
+let nuevaPelicula = {
+  title: "Doctor Strange in the Multiverse of Madness",
+  length: 126,
+  actors: ["Benedict Cumberbatch", "Elizabeth Olsen", "Chiwetel Ejiofor", "Benedict Wong"],
+  awards: 0 // No recibió premios Oscar ni Globos de Oro
+}
+
+let nombres = ["Carlos", "María", "Lucía", "Juan", "Sofía", "Andrés", "Laura", "Miguel", "Ana", "Diego"]
+
+
+// 👇 Resolvé el punto 4.a) 
+
+ 
+
+
+
+
+
+
+// 👇 Resolvé el punto 4.b) 
+
+
+
+
+
+
+
+// 👇 Resolvé el punto 4.c) 
+
+
+
